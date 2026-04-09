946791a5d465acd18029c3a4c10a9cdf413c5a85
Love Danielsson <ldn25003@student.mdu.se>
2026-04-09 13:13:01 +0200

    added draw line and finnished draw screen buffer

--- include/types.h
+++ include/types.h
@@ -3,6 +3,7 @@
 #ifndef TYPES
 #define TYPES
 
+#define FIXED_POINT_DECIMAL_PLACE 16
 typedef uint32_t FixedPoint
 
 #endif
--- src/ascii-draw.c
+++ src/ascii-draw.c
@@ -1,18 +1,45 @@
 #include <math.h>
 #include <types.h>
+#include <stdint.h>
 
 #define WIDTH 30
 #define HEIGHT 30
 
-const char gradient[16] = " `',;!s3S9K$Q%&@"
+const char gradient[16] = " `',;!s3S9K$Q\u25&@"
 
-void drawScreen(FixedPoint* screenBuffer) {
+void drawScreenBuffer(FixedPoint* screenBuffer) {
     char screenTextBuffer[WIDTH * HEIGHT * 2 + 1];
 
     for(int i = 0; i < WIDTH * HEIGHT; i++) {
-        screenTextBuffer[i * 2    ] = round(screenBuffer[i] * 16);
-        screenTextBuffer[i * 2 + 1] = round(screenBuffer[i] * 16);
+        screenTextBuffer[i * 2    ] = gradient[((screenBuffer[i] * 16) & (0xff << FIXED_POINT_DECIMAL_PLACE)) >> FIXED_POINT_DECIMAL_PLACE];
+        screenTextBuffer[i * 2 + 1] = gradient[((screenBuffer[i] * 16) & (0xff << FIXED_POINT_DECIMAL_PLACE)) >> FIXED_POINT_DECIMAL_PLACE];
     }
 
     printf("%s", screenTextBuffer);
+}
+
+void drawLine(FixedPoint* screenBuffer, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, FixedPoint value) {
+    uint32_t k;
+    uint16_t x = x1;
+    uint32_t y = y1;
+
+    if (x1 == x2) {
+        for (uint16_t y = y1; y > y2; y--)   
+            screenBuffer[x + WIDTH * y] = value;
+        for (; y < y2; y++)   
+            screenBuffer[x + WIDTH * y] = value;
+    } else {
+        uint16_t x = x1;
+        uint32_t y = y1 << 16;
+        uint32_t k = ((y2 - y1) << 16) / (x2 - x1);
+        for (; x > x2; x--) {
+            screenBuffer[x + WIDTH * ((y >> 16) & 0xffffffff)] = value;
+            y += k;
+        }
+        for (; x < x2; x++) {
+            screenBuffer[x + WIDTH * ((y >> 16) & 0xffffffff)] = value;
+            y += k;
+        }
+    } 
+
 }
